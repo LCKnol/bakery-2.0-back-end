ad0f2fc982e9ba7d8fd6c39ef1a4371e0d0d7e14
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-04-08 21:29:20 +0200

    Setup directory for start project with example files

--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/controller/WelcomePage.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/controller/WelcomePage.kt
@@ -1,4 +1,4 @@
-package nl.han.oose.colossus.backend.bakery2
+package nl.han.oose.colossus.backend.bakery2.controller
 
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
---
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/service/WelcomeService.kt
@@ -0,0 +1,5 @@
+package nl.han.oose.colossus.backend.bakery2.service
+
+
+class WelcomeService {
+}
