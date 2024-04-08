afcb38e6a14c2ec6a49c3c3e834eddeede7f6e9c
Eduardo-ict <eduardovanbenthum@gmail.com>
2024-04-08 21:17:43 +0200

    spring boot insert

---
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/WelcomePage.kt
@@ -0,0 +1,13 @@
+package nl.han.oose.colossus.backend.bakery2
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class WelcomePage {
+    @GetMapping(value = ["/test"], produces = [MediaType.TEXT_PLAIN_VALUE])
+    fun helloWorld(): String {
+        return "Hello World";
+    }
+}w8
