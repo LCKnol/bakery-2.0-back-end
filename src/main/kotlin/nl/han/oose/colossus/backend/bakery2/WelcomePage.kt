07ba859a6902b7508a87fb6e42ec58db757dfaeb
Eduardo-ict <eduardovanbenthum@gmail.com>
2024-04-08 21:22:11 +0200

    Update WelcomePage.kt

--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/WelcomePage.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/WelcomePage.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class WelcomePage {
-    @GetMapping(value = ["/test"], produces = [MediaType.TEXT_PLAIN_VALUE])
+    @GetMapping(value = ["/hello"], produces = [MediaType.TEXT_PLAIN_VALUE])
     fun helloWorld(): String {
         return "Hello World";
     }
