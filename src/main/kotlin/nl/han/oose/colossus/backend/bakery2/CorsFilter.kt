9ca6239b538cd622464f837a4369223d1238a5d7
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-04-12 09:38:24 +0200

    Added CorsFilter so back-end can recieve requests from front-end

---
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/CorsFilter.kt
@@ -0,0 +1,17 @@
+package nl.han.oose.colossus.backend.bakery2
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+class CorsFilter {
+
+    @Configuration
+    @EnableWebMvc
+    class WebConfig : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/**")
+        }
+    }
+}
