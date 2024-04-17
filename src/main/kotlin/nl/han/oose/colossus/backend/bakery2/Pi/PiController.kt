6eea8bad3dfc33c491cbab3a136b07bdc03a527f
Mees Kouwenberg <MPH.Kouwenberg@student.han.nl>
2024-04-17 15:07:48 +0200

    🩹: Fix for merge

--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/Pi/PiController.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/Pi/PiController.kt
@@ -3,6 +3,8 @@ package nl.han.oose.colossus.backend.bakery2.Pi
 import nl.han.oose.colossus.backend.bakery2.Users.UserService
 import nl.han.oose.colossus.backend.bakery2.dto.PiCollectionDto
 import nl.han.oose.colossus.backend.bakery2.dto.TokenDto
+import nl.han.oose.colossus.backend.bakery2.token.Authenticate
+import nl.han.oose.colossus.backend.bakery2.token.TokenService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -27,7 +29,7 @@ class PiController{
 
     @GetMapping(produces =[MediaType.APPLICATION_JSON_VALUE])
     @Authenticate
-    fun getPis(): ResponseEntity<PiCollectionDTO> {
+    fun getPis(): ResponseEntity<PiCollectionDto> {
         val token = tokenService.getToken()
         val user = userService.getUser(token)
         val pisResponse = piService.getPis(user)
--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/Users/UserController.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/Users/UserController.kt
@@ -3,10 +3,8 @@ package nl.han.oose.colossus.backend.bakery2.Users
 import nl.han.oose.colossus.backend.bakery2.dto.TokenDto
 import nl.han.oose.colossus.backend.bakery2.dto.UserInfoDto
 import org.springframework.beans.factory.annotation.Autowired
-import nl.han.oose.colossus.backend.bakery2.dto.UserInfoDTO
 import nl.han.oose.colossus.backend.bakery2.token.Authenticate
 import nl.han.oose.colossus.backend.bakery2.token.TokenService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -28,9 +26,9 @@ class UserController {
 
     @GetMapping(produces =[MediaType.APPLICATION_JSON_VALUE])
     @Authenticate
-    fun getUserInfo(): ResponseEntity<UserInfoDTO> {
+    fun getUserInfo(): ResponseEntity<UserInfoDto> {
         val token = tokenService.getToken()
-        val user = userService.getUserInfo(token.token)
+        val user = userService.getUserInfo(token)
         return ResponseEntity(user, HttpStatus.OK)
     }
 
