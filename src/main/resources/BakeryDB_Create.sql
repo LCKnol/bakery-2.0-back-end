56a70911e3bd255e3315e0ef946278ee457cb526
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-04-18 14:21:44 +0200

    🎨: Cleanup

--- src/main/resources/BakeryDB_Create.sql
+++ src/main/resources/BakeryDB_Create.sql
@@ -2,23 +2,22 @@
 /* DBMS name:      MySQL 5.0                                    */
 /* Created on:     15-4-2024 15:37:17                           */
 /*==============================================================*/
-USE bakeryDB;
 
-drop table if exists DASHBOARD;
+DROP TABLE IF EXISTS PI;
 
-drop table if exists PI;
+DROP TABLE IF EXISTS DASHBOARD;
 
-drop table if exists ROOM;
+DROP TABLE IF EXISTS TEAMINROOM;
 
-drop table if exists TEAM;
+DROP TABLE IF EXISTS ROOM;
 
-drop table if exists TEAMINROOM;
+DROP TABLE IF EXISTS USERINTEAM;
 
-drop table if exists USER;
+DROP TABLE IF EXISTS TEAM;
 
-drop table if exists USERINTEAM;
+DROP TABLE IF EXISTS USERSESSION;
 
-drop table if exists USERSESSION;
+DROP TABLE IF EXISTS USERS;
 
 /*==============================================================*/
 /* Table: DASHBOARD                                             */
@@ -78,7 +77,7 @@ create table TEAMINROOM
 /*==============================================================*/
 /* Table: USER                                                  */
 /*==============================================================*/
-create table USER
+create table USERS
 (
    USERID               int not null AUTO_INCREMENT,
    FIRSTNAME            varchar(64) not null,
@@ -111,7 +110,7 @@ create table USERSESSION
 );
 
 alter table DASHBOARD add constraint FK_DASHBOARDFROMUSER foreign key (USERID)
-      references USER (USERID) on delete restrict on update restrict;
+      references USERS (USERID) on delete restrict on update restrict;
 
 alter table PI add constraint FK_DASHBOARDONPI foreign key (DASHBOARDID)
       references DASHBOARD (DASHBOARDID) on delete restrict on update restrict;
@@ -126,11 +125,11 @@ alter table TEAMINROOM add constraint FK_TEAMINROOM2 foreign key (ROOMNO)
       references ROOM (ROOMNO) on delete restrict on update restrict;
 
 alter table USERINTEAM add constraint FK_USERINTEAM foreign key (USERID)
-      references USER (USERID) on delete restrict on update restrict;
+      references USERS (USERID) on delete restrict on update restrict;
 
 alter table USERINTEAM add constraint FK_USERINTEAM2 foreign key (TEAMID)
       references TEAM (TEAMID) on delete restrict on update restrict;
 
 alter table USERSESSION add constraint FK_USERSESSION foreign key (USERID)
-      references USER (USERID) on delete restrict on update restrict;
+      references USERS (USERID) on delete restrict on update restrict;
 
