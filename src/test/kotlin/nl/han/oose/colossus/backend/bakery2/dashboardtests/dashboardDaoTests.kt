b581a63f26e63aec6d524cfc3ef6db157f8ef759
Eduardo-ict <ej.vanbenthum@student.han.nl>
2024-04-19 09:36:40 +0200

    🐛 fixed create script loc

--- src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardDaoTests.kt
+++ src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardDaoTests.kt
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 import java.io.InputStreamReader
-import java.sql.Connection
 import java.sql.ResultSet
 
 class dashboardDaoTests {
