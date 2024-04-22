49a01007d5bcfe62018f7786e33b83c730885cd6
Eduardo-ict <ej.vanbenthum@student.han.nl>
2024-04-22 14:20:39 +0200

    update URL -> Url

--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/dashboards/DashboardsDaoImp.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/dashboards/DashboardsDaoImp.kt
@@ -34,7 +34,7 @@ class DashboardsDaoImp : DashboardsDao {
         statement.setInt(1, dashboardDto.getUserId())
         statement.setString(2, dashboardDto.getName())
         statement.setString(3, dashboardDto.getDashboardUrl())
-        statement.setString(4, dashboardDto.getImageURL())
+        statement.setString(4, dashboardDto.getImageUrl())
         statement.executeUpdate()
         statement.close()
     }
@@ -67,7 +67,7 @@ class DashboardsDaoImp : DashboardsDao {
         statement.setInt(1, dashboardDto.getUserId())
         statement.setString(2, dashboardDto.getName())
         statement.setString(3,dashboardDto.getDashboardUrl())
-        statement.setString(4,dashboardDto.getImageURL())
+        statement.setString(4,dashboardDto.getImageUrl())
         statement.setInt(5, dashboardDto.getId())
         statement.executeUpdate()
         statement.close()
