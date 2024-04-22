a7844c9ce92b78a46406f07e95f9c06b019547eb
Luc Knol <LC.Knol@student.han.nl>
2024-04-22 16:01:23 +0200

    🚚: Rename package

--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/dashboards/DashboardsController.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/dashboards/DashboardsController.kt
@@ -1,6 +1,6 @@
 package nl.han.oose.colossus.backend.bakery2.dashboards
 
-import nl.han.oose.colossus.backend.bakery2.Users.UserService
+import nl.han.oose.colossus.backend.bakery2.users.UserService
 import nl.han.oose.colossus.backend.bakery2.dto.DashboardCollectionDto
 import nl.han.oose.colossus.backend.bakery2.dto.DashboardDto
 import nl.han.oose.colossus.backend.bakery2.token.Authenticate
