ec51eb79db4ea7b47e7dac4337dae7bc7faaeab5
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-04-22 20:02:01 +0200

    ✅ update test

--- src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardControllerTests.kt
+++ src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardControllerTests.kt
@@ -1,10 +1,12 @@
 package nl.han.oose.colossus.backend.bakery2.dashboardtests
 
 import junit.framework.Assert.assertEquals
+import nl.han.oose.colossus.backend.bakery2.Users.UserService
 import nl.han.oose.colossus.backend.bakery2.dashboards.DashboardsController
 import nl.han.oose.colossus.backend.bakery2.dashboards.DashboardsService
 import nl.han.oose.colossus.backend.bakery2.dto.DashboardCollectionDto
 import nl.han.oose.colossus.backend.bakery2.dto.DashboardDto
+import nl.han.oose.colossus.backend.bakery2.token.TokenService
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -19,13 +21,21 @@ class dashboardControllerTests {
 
     private lateinit var dashboardsService: DashboardsService
 
+    private lateinit var  tokenService: TokenService
+
+    private lateinit var userService: UserService
+
 
     @Test
     @BeforeEach
     fun setup() {
         sut = DashboardsController()
         dashboardsService = mock(DashboardsService::class.java)
+        tokenService = mock(TokenService::class.java)
+        userService = mock(UserService::class.java)
         sut.setDashboardsService(dashboardsService)
+        sut.setTokenService(tokenService)
+        sut.setUserService(userService)
     }
 
 
@@ -53,18 +63,5 @@ class dashboardControllerTests {
         verify(dashboardsService).addDashboard(dashboard)
     }
 
-    @Test
-    fun testUpdateDashboardWorksCorrectly() {
-        // Arrange
-        val dashboard: DashboardDto = DashboardDto(1, "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "meme", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F013%2F480%2F841%2Foriginal%2Fcartoon-illustration-of-mother-and-baby-ducks-vector.jpg&f=1&nofb=1&ipt=44a60e01529dd6fb9a7ee5510fd043aa451bbf13599518a5ae912f2499fd38a8&ipo=images", 1)
-        doNothing().`when`(dashboardsService).editDashboard(dashboard,1)
-
-        // Act
-        val result = sut.editDashboard(dashboard).statusCode
-
-        // Assert
-        verify(dashboardsService).editDashboard(dashboard,1)
-        Assertions.assertEquals(204, result.value())
-    }
 
 }
