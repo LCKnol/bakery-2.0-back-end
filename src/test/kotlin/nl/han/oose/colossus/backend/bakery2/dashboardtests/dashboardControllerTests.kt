691bff826f04b96b7277baf56418ef983dcfc8b5
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-04-22 20:04:21 +0200

    ✅ final update test

--- src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardControllerTests.kt
+++ src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardControllerTests.kt
@@ -1,7 +1,7 @@
 package nl.han.oose.colossus.backend.bakery2.dashboardtests
 
 import junit.framework.Assert.assertEquals
-import nl.han.oose.colossus.backend.bakery2.Users.UserService
+import nl.han.oose.colossus.backend.bakery2.users.UserService
 import nl.han.oose.colossus.backend.bakery2.dashboards.DashboardsController
 import nl.han.oose.colossus.backend.bakery2.dashboards.DashboardsService
 import nl.han.oose.colossus.backend.bakery2.dto.DashboardCollectionDto
