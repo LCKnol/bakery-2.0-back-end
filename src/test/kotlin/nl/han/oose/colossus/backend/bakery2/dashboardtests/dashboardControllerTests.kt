0e8383b133cedf7cb504731f0d5f42b9f0f1bf59
Luc Knol <LC.Knol@student.han.nl>
2024-04-23 10:48:00 +0200

    ✅: Add dashboard controller test for delete

--- src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardControllerTests.kt
+++ src/test/kotlin/nl/han/oose/colossus/backend/bakery2/dashboardtests/dashboardControllerTests.kt
@@ -6,27 +6,24 @@ import nl.han.oose.colossus.backend.bakery2.dashboards.DashboardsController
 import nl.han.oose.colossus.backend.bakery2.dashboards.DashboardsService
 import nl.han.oose.colossus.backend.bakery2.dto.DashboardCollectionDto
 import nl.han.oose.colossus.backend.bakery2.dto.DashboardDto
+import nl.han.oose.colossus.backend.bakery2.dto.UserDto
 import nl.han.oose.colossus.backend.bakery2.token.TokenService
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.mockito.Mockito.*
-
-
+import org.springframework.http.HttpStatus
 
 class dashboardControllerTests {
-
 
     private lateinit var sut: DashboardsController
 
     private lateinit var dashboardsService: DashboardsService
 
-    private lateinit var  tokenService: TokenService
+    private lateinit var tokenService: TokenService
 
     private lateinit var userService: UserService
 
-
-    @Test
     @BeforeEach
     fun setup() {
         sut = DashboardsController()
@@ -38,10 +35,8 @@ class dashboardControllerTests {
         sut.setUserService(userService)
     }
 
-
     @Test
-    fun TestGetAllDashboardWorksCorrectly() {
-
+    fun testGetAllDashboardWorksCorrectly() {
         // Arrange
         val dashboard: DashboardCollectionDto = DashboardCollectionDto()
         `when`(dashboardsService.getAllDashboards()).thenReturn(dashboard)
@@ -51,11 +46,11 @@ class dashboardControllerTests {
         assertEquals(200, response)
         verify(dashboardsService).getAllDashboards()
     }
-    @Test
-    fun TestaddDashboardsCorrectly() {
 
+    @Test
+    fun testAddDashboardsCorrectly() {
         // Arrange
-        val dashboard: DashboardDto = DashboardDto(1,"test","test","test",1)
+        val dashboard: DashboardDto = DashboardDto(1, "test", "test", "test", 1)
         // Act
         val response = sut.addDashboards(dashboard).statusCode.value()
         // Assert
@@ -63,5 +58,24 @@ class dashboardControllerTests {
         verify(dashboardsService).addDashboard(dashboard)
     }
 
+    @Test
+    fun testDeleteDashboardsCorrectly() {
+        // Arrange
+        val mockToken = "9e8b4196-b691-45f9-8fb7-acec4f0f9a4b"
+        val dashboardId = 2
+        val mockUserId = 1
 
+        `when`(tokenService.getToken()).thenReturn(mockToken)
+        `when`(userService.getUserId(mockToken)).thenReturn(mockUserId)
+
+        // Act
+        val response = sut.deleteDashboard(dashboardId).statusCode
+
+        // Assert
+        assertEquals(HttpStatus.OK, response)
+
+        verify(tokenService).getToken()
+        verify(userService).getUserId(mockToken)
+        verify(dashboardsService).deleteDashboard(dashboardId, mockUserId)
+    }
 }
