609537a95b72db54776722f0000832f07b8589d3
Eduardo-ict <141565377+Ewawo@users.noreply.github.com>
2024-05-13 13:07:59 +0200

    Update ApplicationTests.kt

--- src/test/kotlin/nl/han/oose/colossus/backend/bakery2/ApplicationTests.kt
+++ src/test/kotlin/nl/han/oose/colossus/backend/bakery2/ApplicationTests.kt
@@ -1,9 +1,8 @@
 package nl.han.oose.colossus.backend.bakery2
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+
 class ApplicationTests {
 
     @Test
