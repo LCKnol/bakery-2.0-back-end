eabfc90bc1344e7b0048c09fc34da645eafae985
Mees Kouwenberg <MPH.Kouwenberg@student.han.nl>
2024-05-24 14:37:33 +0200

    Update TeamDaoImp.kt

--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/teams/TeamDaoImp.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/teams/TeamDaoImp.kt
@@ -25,6 +25,19 @@ class TeamDaoImp: TeamDao {
     }
 
     @Throws(ServerErrorException::class)
+    override fun getTeamsFromUser(userId: Int): TeamCollectionDto {
+        val query = "SELECT * FROM TEAM WHERE TEAMID IN (SELECT TEAMID FROM USERINTEAM WHERE USERID = ?)"
+        val connection = dbConnection.getConnection()
+        val preparedStatement = connection.prepareStatement(query)
+        preparedStatement.setInt(1, userId)
+        val resultSet = preparedStatement.executeQuery()
+        val teams = teamMapper.mapUserTeams(resultSet)
+        preparedStatement.close()
+        connection.close()
+        return teams
+    }
+
+    @Throws(ServerErrorException::class)
     override fun getTeams(userId: Int): TeamCollectionDto {
         val query = "SELECT * FROM TEAM WHERE TEAMID IN (SELECT TEAMID FROM USERINTEAM WHERE USERID = ?)"
         val connection = dbConnection.getConnection()
