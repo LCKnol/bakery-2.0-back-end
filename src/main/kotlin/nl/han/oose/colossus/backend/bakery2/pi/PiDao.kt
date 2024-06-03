3e0784c9d0e35dea3f318c3cabeec4596eda44eb
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-06-03 14:57:36 +0200

    ✅ Fixed inverted params

--- src/main/kotlin/nl/han/oose/colossus/backend/bakery2/pi/PiDao.kt
+++ src/main/kotlin/nl/han/oose/colossus/backend/bakery2/pi/PiDao.kt
@@ -19,7 +19,7 @@ interface PiDao {
     fun updatePiIp(piSignUpRequestDto: PiSignUpRequestDto)
     fun getPi(piId: Int): PiDto?
     fun getMacAddress(piId: Int):String
-    fun assignDashboard(dashboardId: Int, piId: Int)
+    fun assignDashboard(piId: Int,dashboardId: Int)
     fun getPi(piId: Int?, macAddress: String?): PiDto?
     fun updateStatus(status: String, piId: Int)
 }
